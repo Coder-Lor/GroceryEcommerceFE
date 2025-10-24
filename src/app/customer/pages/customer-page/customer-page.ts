52b1d73d937da4e327e329d7dad0cd480a1e0362
Hung Tran <tdh130204@gmail.com>
2025-10-24 16:37:26 +0700

    xử lý conflict

--- src/app/customer/pages/customer-page/customer-page.ts
+++ src/app/customer/pages/customer-page/customer-page.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { Header } from "../../shared/components/header/header";
-import { RouterModule } from "@angular/router";
-import { Footer } from "../../shared/components/footer/footer";
+import { RouterModule } from '@angular/router';
+import { Footer } from '../../shared/components/footer/footer';
+import { Header } from '../../shared/components/header/header';
 
 @Component({
   selector: 'app-customer-page',
+  standalone: true,
   imports: [Header, RouterModule, Footer],
   templateUrl: './customer-page.html',
   styleUrl: './customer-page.scss',
 })
-export class CustomerPage {
-
-}
+export class CustomerPage {}
