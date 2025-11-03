d34183484c44260dedac87615c8bd165a229d363
tuancter <tuanphuong130204@gmail.com>
2025-11-03 17:40:29 +0700

    edited

--- src/app/admin/users/users-page.component.ts
+++ src/app/admin/users/users-page.component.ts
@@ -1,8 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ToastModule, Toast } from 'primeng/toast';
-import { Button } from "primeng/button";
-import { MessageService } from 'primeng/api';
-import { RippleModule, Ripple } from 'primeng/ripple';
+
 
 @Component({
   selector: 'app-users-page',
@@ -11,13 +8,7 @@ import { RippleModule, Ripple } from 'primeng/ripple';
   styles: [
     `:host { display: block; }`
   ],
-  imports: [Toast, Button, Ripple]
+  imports: []
 })
 export class UsersPageComponent {
-  private messageService = inject(MessageService);
-
-  show(): void {
-    this.messageService.add({ severity: 'error', summary: 'Info', detail: 'Message Content', life: 3000 });
-  }
-
 }
