5eeb043c8c06a76fd0106e89710a12bcb25804b5
tuancter <tuanphuong130204@gmail.com>
2026-01-05 16:02:06 +0700

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -54,8 +54,8 @@ export const appConfig: ApplicationConfig = {
     { provide: LOCALE_ID, useValue: 'vi-VN' },
     MessageService,
     ConfirmationService,
-    { provide: API_BASE_URL, useValue: 'https://localhost:44394' },
-    // { provide: API_BASE_URL, useValue: "https://groceryecommercebe-bscbhpd4bgcma3gf.southeastasia-01.azurewebsites.net" },
+    // { provide: API_BASE_URL, useValue: 'https://localhost:44394' },
+    { provide: API_BASE_URL, useValue: "https://groceryecommercebe-bscbhpd4bgcma3gf.southeastasia-01.azurewebsites.net" },
     { provide: API_KEY, useValue: "AIzaSyBF8oMhqlgIExmjfNM4D41V8PWQJCbG25s" }
   ],
 };
